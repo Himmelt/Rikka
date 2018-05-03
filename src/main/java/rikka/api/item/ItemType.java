17840b15a4fad69458d484fec529332ea2551100
Himmelt <master@void-3.cn>
2018-05-03 22:03:44 +0800

    update itemstack

--- src/main/java/rikka/api/item/ItemType.java
+++ src/main/java/rikka/api/item/ItemType.java
@@ -2,7 +2,6 @@ package rikka.api.item;
 
 import rikka.api.CatalogType;
 import rikka.api.block.BlockType;
-import rikka.api.data.Property;
 import rikka.api.text.translation.Translatable;
 import rikka.api.util.annotation.CatalogedBy;
 
@@ -17,7 +16,5 @@ public interface ItemType extends CatalogType, Translatable {
     String getName();
 
     int getMaxStackQuantity();
-
-    <T extends Property<?, ?>> Optional<T> getDefaultProperty(Class<T> propertyClass);
 
 }
