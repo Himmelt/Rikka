839ad19f5ceb8eafb7ef0f5b84b16b6ea2ae3cff
Himmelt <master@void-3.cn>
2018-05-19 09:26:58 +0800

    fix SpectralArrow & BlockType ORANGE_GLAZED_TERRACOTTA

--- src/main/java/rikka/sponge/entity/projectile/SpongeSpectralArrow.java
+++ src/main/java/rikka/sponge/entity/projectile/SpongeSpectralArrow.java
@@ -1,13 +1,10 @@
 package rikka.sponge.entity.projectile;
 
-import org.spongepowered.api.entity.projectile.arrow.Arrow;
 import org.spongepowered.api.entity.projectile.arrow.SpectralArrow;
 import rikka.api.entity.projectile.ISpectralArrow;
 
-// TODO SpectralArrow extends Arrow
-public final class SpongeSpectralArrow extends SpongeArrow/*<SpectralArrow>*/ implements ISpectralArrow {
+public final class SpongeSpectralArrow extends SpongeArrow<SpectralArrow> implements ISpectralArrow {
     public SpongeSpectralArrow(SpectralArrow source) {
-        // TODO remove force trans type
-        super((Arrow) source);
+        super(source);
     }
 }
